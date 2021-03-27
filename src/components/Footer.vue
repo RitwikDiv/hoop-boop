718dff9bd604e29cd58f587982bcf8726f9231d4
ritwikdiv <ritwik.edu@gmail.com>
2021-03-26 20:49:52 -0400

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="footer mt-5"></footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.footer {
+  bottom: 20px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <router-link to="/home">{{ name }}</router-link>
+    <router-link to="/about"> About </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      name: "Hoop Boop",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
